00168c5c4563d9a5eeed5d404858835ca6e7e68c
Emilyjones345 <105925745+Emilyjones345@users.noreply.github.com>
2025-04-13 02:17:23 +0530

    realtimenoti

--- backend/config/huggingface.js
+++ backend/config/huggingface.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 
 const HUGGINGFACE_API_TOKEN = process.env.HUGGINGFACE_API_TOKEN;
 
+
 const huggingfaceInstance = axios.create({
   baseURL:
     "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.1",
